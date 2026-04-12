aa9e1a452c9878a40c9c10aea6f97256bfbd704d
comae <jannis.koberg@icloud.com>
2026-04-12 14:14:52 +0200

    fix: embed WebView2 bootstrapper, add macOS/Linux bundle config, bump to 0.1.1

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-mod models;
-
 fn main() {
+    #[cfg(target_os = "linux")]
+    std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
     open_dnd_lib::run()
 }
